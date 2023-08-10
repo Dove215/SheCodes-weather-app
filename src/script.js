1de87b516f79b364e6a230cc563f8a0addd12816
Dovile <dovi.ambr@gmail.com>
2023-08-10 21:12:03 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -64,7 +64,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
